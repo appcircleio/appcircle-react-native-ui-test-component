310c59a9a1d780cde7eb34e5069a0fb769e4ae75
boztopuz <boztopuz@gmail.com>
2024-10-30 19:15:46 +0300

    Added readme file and feedback fixed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Appcircle React Native UI Test Component
+
+Run React Native UI Tests with Detox
+
+## Required Input Variables
+
+- `$AC_REPOSITORY_DIR`: Specifies the cloned repository directory.
+- `$AC_OUTPUT_DIR`: Specify the output directory for test results.
+- `$AC_RN_DETOX_CONFIGURATION`: Specify the Detox configuration name to used when building and running the tests.
+
+## Output Variables
+
+- `$AC_TEST_RESULT_PATH`: Path to the test result file.
--- main.rb
+++ main.rb
@@ -1,15 +1,13 @@
-require 'find'
-require 'json'
 require 'open3'
 require 'os'
 
 def env_has_key(key)
-    return (ENV[key] == nil || ENV[key] == "") ? nil : ENV[key]
+  !ENV[key].nil? && ENV[key] != '' ? ENV[key] : abort("Missing #{key}.")
 end
 
-$output_path = env_has_key("AC_OUTPUT_DIR") || abort("Missing AC_OUTPUT_DIR.")
-$repo_path = env_has_key("AC_REPOSITORY_DIR") || abort("Missing AC_REPOSITORY_DIR.")
-$detox_configuration = env_has_key("AC_RN_DETOX_CONFIGURATION") || abort("Missing AC_RN_DETOX_CONFIGURATION.")
+$output_path = env_has_key("AC_OUTPUT_DIR")
+$repo_path = env_has_key("AC_REPOSITORY_DIR")
+$detox_configuration = env_has_key("AC_RN_DETOX_CONFIGURATION")
 
 def run_command(command)
     puts "@@[command] #{command}"
@@ -24,7 +22,6 @@ def run_command(command)
       status = wait_thr.value
     end
   
-    # Return success status and error message (if any)
     if status.success?
       return true, stdout_str
     else
@@ -33,46 +30,35 @@ def run_command(command)
 end
 
 def runTests
-    results = []
 
-        if OS.mac?
-            results << run_command("brew tap wix/brew && brew install applesimutils")
-        end 
+    if OS.mac?
+      run_command("brew tap wix/brew && brew install applesimutils")
+    end 
 
-        yarn_or_npm = "npm"
-        if File.file?("#{$repo_path}/yarn.lock")
-            yarn_or_npm = "yarn"
-        end
-
-        puts "Detox Configuration used is #{$detox_configuration}"
-
-        results << run_command("cd #{$repo_path} && #{yarn_or_npm} detox clean-framework-cache")
-        results << run_command("cd #{$repo_path} && #{yarn_or_npm} detox build-framework-cache --configuration #{$detox_configuration}")
-
-        results << run_command("cd #{$repo_path} && #{yarn_or_npm} detox build --configuration #{$detox_configuration}")
-        results << run_command("cd #{$repo_path} && #{yarn_or_npm} detox test --configuration #{$detox_configuration} --take-screenshots all")
-        
-        # copy test results to output directory for downloadable artifacts
-        results << run_command("cp #{$repo_path}/test-reports/*-report.xml #{$output_path}")
-
-        # copy e2e test reports to output directory for downloadable artifacts
-        results << run_command("cp -rp #{$repo_path}/artifacts/* #{$output_path}/test_attachments")
-        
-
-        # Write AC_TEST_RESULT_PATH reserved variable to the ENV file for test report component
-        File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
-            f.puts "AC_TEST_RESULT_PATH=#{$output_path}"
-        end
-   
-      # Check results and handle failure
-    results.each_with_index do |(success, result), index|
-        unless success
-          abort("Command #{index + 1} failed: #{result}")
-        end
+    yarn_or_npm = "npm"
+    if File.file?("#{$repo_path}/yarn.lock")
+      yarn_or_npm = "yarn"
     end
-    
-    "Tests completed successfully"
-end
 
+    puts "Detox Configuration used is #{$detox_configuration}"
+
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox clean-framework-cache")
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox build-framework-cache --configuration #{$detox_configuration}")
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox build --configuration #{$detox_configuration}")
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox test --configuration #{$detox_configuration} --take-screenshots all")
+    run_command("cp #{$repo_path}/test-reports/*-report.xml #{$output_path}")    
+    run_command("cp -rp #{$repo_path}/artifacts/* #{$output_path}/test_attachments")     
+    
+    File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
+       f.puts "AC_TEST_RESULT_PATH=#{$output_path}"
+    end
+
+    results.each_with_index do |(success, result), index|
+      unless success
+        abort("Command #{index + 1} failed: #{result}")
+      end
+    end
+    puts "Tests completed successfully"
+end
 
 runTests()
