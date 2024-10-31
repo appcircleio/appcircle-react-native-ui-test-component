bd8c861978061f8b35be7b24bc51d5a52ae88343
boztopuz <boztopuz@gmail.com>
2024-10-31 14:32:56 +0300

    Fixed runcommand

--- main.rb
+++ main.rb
@@ -16,23 +16,35 @@ $output_path = env_has_key("AC_OUTPUT_DIR")
 $repo_path = env_has_key("AC_REPOSITORY_DIR")
 $detox_configuration = env_has_key("AC_RN_DETOX_CONFIGURATION")
 
-def run_command(command)
-  puts "@@[command] #{command}"
-  stdout_str, stderr_str, status = Open3.capture3(command)
+$exit_status_code = 0
+def run_command(command, skip_abort)
+    puts "@@[command] #{command}"
+    status = nil
+    stdout_str = nil
+    stderr_str = nil
 
-  if status.success?
-    puts stdout_str unless stdout_str.empty?
-    puts stderr_str unless stderr_str.empty?
-    return stderr_str.empty? ? stdout_str : stderr_str
-  else
-    return stderr_str
-  end
+    Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+        stdout.each_line do |line|
+            puts line
+        end
+        stdout_str = stdout.read
+        stderr_str = stderr.read
+        status = wait_thr.value
+    end
+
+    unless status.success?
+        puts stderr_str
+        unless skip_abort
+            exit -1
+        end
+        $exit_status_code = -1
+    end
 end
 
 def runTests
 
     if OS.mac?
-      run_command("brew tap wix/brew && brew install applesimutils")
+      run_command("brew tap wix/brew && brew install applesimutils", false)
     end 
 
     yarn_or_npm = "npm"
@@ -42,12 +54,12 @@ def runTests
 
     puts "Detox Configuration used is #{$detox_configuration}"
 
-    run_command("cd #{$repo_path} && #{yarn_or_npm} detox clean-framework-cache")
-    run_command("cd #{$repo_path} && #{yarn_or_npm} detox build-framework-cache --configuration #{$detox_configuration}")
-    run_command("cd #{$repo_path} && #{yarn_or_npm} detox build --configuration #{$detox_configuration}")
-    run_command("cd #{$repo_path} && #{yarn_or_npm} detox test --configuration #{$detox_configuration} --take-screenshots all")
-    run_command("cp #{$repo_path}/test-reports/*-report.xml #{$output_path}")    
-    run_command("cp -rp #{$repo_path}/artifacts/* #{$output_path}/test_attachments")     
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox clean-framework-cache", true)
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox build-framework-cache --configuration #{$detox_configuration}", true)
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox build --configuration #{$detox_configuration}", false)
+    run_command("cd #{$repo_path} && #{yarn_or_npm} detox test --configuration #{$detox_configuration} --take-screenshots all", true)
+    run_command("cp #{$repo_path}/test-reports/*-report.xml #{$output_path}", false)    
+    run_command("cp -rp #{$repo_path}/artifacts/* #{$output_path}/test_attachments" false)     
     
     File.open(ENV['AC_ENV_FILE_PATH'], 'a') do |f|
        f.puts "AC_TEST_RESULT_PATH=#{$output_path}"
@@ -57,3 +69,4 @@ def runTests
 end
 
 runTests()
+exit $exit_status_code
